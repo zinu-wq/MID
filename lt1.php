ac2e52ff4be1913567e5a109add84658eb398117
ZarirZaman <zarir.zaman.jzs@gmail.com>
2025-11-16 09:25:59 +0600

    change

--- lt1.php
+++ lt1.php
@@ -1,4 +1,15 @@
 <!DOCTYPE HTML>
-<html>
-    hello world!!
-</html>
+<html lange="en">
+    <head>
+        <title>JS</title>
+        <head>
+            <body>
+                <h1>This is js lab</h1>
+                <script>
+                    document.write("Hello World");
+                    console.log("Hello i am the console");
+                    //alert("hello");
+                    //variable
+                    var name = "John";
+                    let age = 25;
+    
